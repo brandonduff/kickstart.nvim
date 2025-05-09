c8ad2952302131ddad829ff38820149e82181418
Brandon Duff <brandon@mechanical-orchard.com>
2025-05-09 18:14:06 -0400

    some improvements
    - enable custom plugins
    - enable neo-tree
    - fix neotest binding

--- init.lua
+++ init.lua
@@ -1047,14 +1047,14 @@ require('lazy').setup({
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
@@ -1083,7 +1083,7 @@ require('lazy').setup({
 })
 
 -- neotest
-vim.keymap.set('n', '<C-t><C-n>', ':w <bar> TestNearest<cr>')
+vim.keymap.set('n', '<C-t><C-n>', ':w <bar> Neotest run<cr>')
 vim.keymap.set('n', '<C-t><C-f>', ':lua require("neotest").run.run(vim.fn.expand("%"))<cr>')
 vim.keymap.set('n', '<C-t><C-s>', ':Neotest summary<cr>')
 vim.keymap.set('n', '<C-t><C-o>', ':Neotest output<cr>')
