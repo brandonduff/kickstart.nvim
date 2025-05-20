67a436fac4d9f594f9f96035d99b6d4a331ffecb
Brandon Duff <brandon@mechanical-orchard.com>
2025-05-20 17:56:09 -0400

    Add goose.nvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -166,4 +166,19 @@ return {
       pin_version = nil,
     },
   },
+  {
+    'azorng/goose.nvim',
+    config = function()
+      require('goose').setup {}
+    end,
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      {
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          anti_conceal = { enabled = false },
+        },
+      },
+    },
+  },
 }
